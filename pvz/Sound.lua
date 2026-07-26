0a9ca7b37ae2fd352433fca06dad6ec6fffe3ca9
ashley <84390581+inky03@users.noreply.github.com>
2026-07-26 13:09:41 -0600

    Update Sound.lua
    cleanup

--- pvz/Sound.lua
+++ pvz/Sound.lua
@@ -4,17 +4,21 @@ Sound.volume = 1
 
 function Sound.play(path, deviation, pitch, kind, autoPlay)
 	if not path or path == '' then return nil end
+	
 	local path = (type(path) == 'table' and random.object(path) or path)
 	local snd = Cache.sound(path, false, kind)
+	
 	if snd then
-		local snd = snd:clone()
-		local pitch = (pitch or 1)
-		local deviation = (deviation or 0)
+		local snd, pitch, deviation = snd:clone(), (pitch or 1), (deviation or 0)
+		
 		snd:setPitch(random.number(pitch, pitch + deviation / 100))
 		snd:setVolume(Sound.volume)
+		
 		if autoPlay ~= false then snd:play() end
+		
 		return snd
 	end
+	
 	return nil
 end
 
