696c4db68f1b934278b54600bf7e2c5a7dd1176a
ashley <84390581+inky03@users.noreply.github.com>
2026-07-27 05:07:56 -0600

    Update main.lua
    tin y cleanup

--- main.lua
+++ main.lua
@@ -38,12 +38,6 @@ function love.load(arguments)
 	debugInfo:setLayerColor('Main')
 	
 	debugCanvas = love.graphics.newCanvas(400, 200)
-	
-	--[[
-	grasswalk = love.audio.newSource('resources/love/music/grasswalk.mp3', 'stream')
-	grasswalk:setLooping(true)
-	grasswalk:play()
-	]]
 end
 
 function love.mousepressed(mouseX, mouseY, button, isTouch, presses)
@@ -118,16 +112,14 @@ function love.run()
 	
 	local accumulator = 0
 	return function()
-		if love.event then -- Process events.
-			love.event.pump()
-			for name, a,b,c,d,e,f in love.event.poll() do
-				if name == "quit" then
-					if not love.quit or not love.quit() then
-						return a or 0
-					end
+		love.event.pump()
+		for name, a,b,c,d,e,f in love.event.poll() do
+			if name == 'quit' then
+				if not love.quit or not love.quit() then
+					return (a or 0)
 				end
-				love.handlers[name](a,b,c,d,e,f)
 			end
+			love.handlers[name](a,b,c,d,e,f)
 		end
 		
 		local redraw = false
@@ -160,9 +152,9 @@ function love.run()
 		if redraw and love.graphics and love.graphics.isActive() then
 			love.graphics.origin()
 			love.graphics.clear(love.graphics.getBackgroundColor())
-
+			
 			if love.draw then love.draw() end
-
+			
 			love.graphics.present()
 		end
 		
@@ -247,7 +239,7 @@ function drawDebug()
 	while drawtime[1] < time - 1 do
 		table.remove(drawtime, 1)
 	end
-	local fps = math.round((fpsCount + #drawtime) / 2)
+	local fps = math.min(math.round((fpsCount + #drawtime) / 2), flags.maxFramerate)
 	fpsCount = #drawtime
 	
 	love.graphics.setCanvas(debugCanvas)
